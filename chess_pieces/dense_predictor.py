7841475e7e251257ea721e2bfeec54ad63985366
Lorenzo <lorys_23@hotmail.it>
2017-01-31 15:35:05 +0000

    Added KMeans

--- chess_pieces/dense_predictor.py
+++ chess_pieces/dense_predictor.py
@@ -5,10 +5,8 @@ from sklearn.externals import joblib
 from test_dense import myPredict
 
 
-img = cv2.imread('white_pawn27.png')
+img = cv2.imread('window_132.jpeg')
 print myPredict(img)
 
-
-
-# cv2.imshow('img', img)
-# cv2.waitKey(0)
+cv2.imshow('img',img)
+cv2.waitKey(0)
