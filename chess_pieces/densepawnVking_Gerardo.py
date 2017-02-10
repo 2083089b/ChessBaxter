9e823872199cc057c2c14ce95bbf5a6efe90fca5
Gerardo Aragon-Camarasa <gerardo.aragoncamarasa@glasgow.ac.uk>
2017-02-10 16:36:10 +0000

    updated code from gerardo, check gerardo's py files

--- chess_pieces/densepawnVking_Gerardo.py
+++ chess_pieces/densepawnVking_Gerardo.py
@@ -1,19 +1,18 @@
 import numpy as np
 import cv2
-from sklearn import svm
-from sklearn.externals import joblib
-import pickle
-from sklearn.linear_model import LogisticRegression
-from sklearn import cross_validation
 
 from extra_tools import extract_HOG, chess_train
+from scipy.stats import itemfreq
 
-clf = chess_train('descriptors/HOG', ['pawn', 'king','queen','knight','bishop','rock'], train_mod="SVC_linear")
+# 'queen','knight','bishop','rock',
+clf_PKS = chess_train('descriptors/HOG', ['pawn', 'king', 'queen','knight','bishop','rock', 'square'], train_mod="LR")
+# clf_PKS = chess_train('descriptors/HOG', ['pawn', 'king', 'square'], train_mod="LR")
+# clf_QkS = chess_train('descriptors/HOG', ['queen', 'knight', 'square'], train_mod="LR")
+# clf_BRS = chess_train('descriptors/HOG', ['bishop', 'rock', 'square'], train_mod="LR")
 print "Training done!"
 
-
-img = cv2.imread("pawn2.png")
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img = cv2.imread("king8.png")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # img = cv2.drawKeypoints(img,kp,img,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
@@ -24,29 +23,52 @@ np.savetxt('test2.txt', des1, delimiter=',')
 
 des1 = np.float32(des1)
 
-
 # cv2.imshow('img', img)
 # cv2.waitKey(0)
 
-prediction = clf.predict(des1)
-print prediction
-pred_prob = clf.predict_proba(des1)
-print pred_prob
+# label = ["Pawn", "King", "Square", "Queen", "Knight", "Square", "Bishop", "Rook", "Square"]
+label = ["Pawn", "King", "Queen", "Knight", "Bishop", "Rook", "Square"]
+models = [clf_PKS]#, clf_QkS, clf_BRS]
 
-values = []
-counter = 0
+print ""
+print "************************"
+c_val = []
+c_prob = []
+for i, clf in enumerate(models):
+    prediction = clf.predict(des1)
+    freq = itemfreq(prediction)
+    c_val.append(freq[:,1].tolist())
+    print freq
+    pred_prob = clf.predict_proba(des1)
+    # print pred_prob
+    print "************************"
+    out = pred_prob.mean(axis=0)
+    print "Out: " + str(out)
+    c_prob.append(out.tolist())
+    print "************************"
+    print "************************"
+    print ""
 
-for entry in pred_prob:
-	if float(prediction[counter]) == 0.0:
-		values.append(entry[0])
-	counter += 1
+# NOT WORKING: NEED TO FIX FREQUENCY ACC TO INCLUDE ALL CLASSES PER CLASSIFIER
+# freq_class = np.array(c_val).flatten()
+# prob_class = np.array(c_prob).flatten()
+# prod = freq_class * prob_class
+# idx_c = freq_class.argmax()
+# print "Found: " + label[idx_c]
 
+# values = []
+# counter = 0
+
+# for entry in pred_prob:
+#     if float(prediction[counter]) == 0.0:
+#         values.append(entry[0])
+#     counter += 1
 
 # Calculate the mean
 # print "Mean: " + str(sum(values)/len(values))
 
-out = pred_prob.mean(axis=0)
-print "Out: " + str(out)
+# out = pred_prob.mean(axis=0)
+# print "Out: " + str(out)
 
 # total = 0
 # counter = 0
--- chess_pieces/descriptors_creator-Gerardo.py
+++ chess_pieces/descriptors_creator-Gerardo.py
@@ -5,50 +5,48 @@ import glob
 from sklearn.externals import joblib
 from extra_tools import extract_HOG
 
+folders_names = ['bishop', 'king', 'knight', 'pawn', 'queen', 'rock', 'square']
 
-folders_names = ['bishop','king','knight','pawn','queen','rock','square']
-
-words_per_class = 200
+words_per_class = 150
 
 print "\nNumber of descriptors:\n"
 # Go through each folder containing pictures of the chess pieces
 for folder_name in folders_names:
-	# Read all images in the current folder
-	print folder_name
-	images = []
+    # Read all images in the current folder
+    print folder_name
+    images = []
 
+    for filename in glob.glob('cropped_pictures/' + folder_name + '/*.png'):
+        image = cv2.imread(filename)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        images.append(gray)
 
-	for filename in glob.glob('cropped_pictures/'+folder_name+'/*.png'):
-		image = cv2.imread(filename)
-		gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-		images.append(gray)
+    descriptors = []
 
-	descriptors = []
+    for image in images:
+        # add hog
+        kp, des1 = extract_HOG(image, 0)
 
-	for image in images:
-		# add hog
-		kp, des1 = extract_HOG(image, 0)
+        for c, des in enumerate(des1):
+            descriptors.append(des)
 
-		for c, des in enumerate(des1):
-			descriptors.append(des)
+    print folder_name + ": " + str(len(descriptors))
+    # print descriptors
+    descs = np.float32(descriptors)
+    joblib.dump(descs, 'descriptors/HOG/' + folder_name + '/' + folder_name + '.pkl')
 
-	# print folder_name + ": " + str(len(descriptors))
-	# print descriptors
-	descs = np.float32(descriptors)
-	joblib.dump(descs,'descriptors/HOG/'+folder_name+'/'+folder_name+'.pkl')
+    print("length of descriptors: " + str(len(descs[0])))
+    print("number of descriptors extracted: " + str(len(descs)))
 
-	# print("length of descriptors: " + str(len(descs[0])))
-	# print("number of descriptors extracted: " + str(len(descs)))
-	#
-	# criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-	# try:
-	# 	ret, label, centers = cv2.kmeans(np.array(descs), words_per_class, criteria, 10,
-	# 									 cv2.KMEANS_RANDOM_CENTERS)
-	# except:
-	# 	ret, label, centers = cv2.kmeans(np.array(descs), words_per_class, None, criteria, 10,
-	# 									 cv2.KMEANS_RANDOM_CENTERS)
-	#
-	# print("From " + str(len(descs)) + " descriptors to " + str(len(centers)) + " descriptors \n")
-	# descs = np.float32(centers)
+    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    try:
+        ret, label, centers = cv2.kmeans(np.array(descs), words_per_class, criteria, 10,
+                                         cv2.KMEANS_RANDOM_CENTERS)
+    except:
+        ret, label, centers = cv2.kmeans(np.array(descs), words_per_class, None, criteria, 10,
+                                         cv2.KMEANS_RANDOM_CENTERS)
 
-	np.savetxt('descriptors/HOG/'+folder_name+'/'+folder_name+'.vocab', descs, delimiter=',')
+    print("From " + str(len(descs)) + " descriptors to " + str(len(centers)) + " descriptors \n")
+    descs = np.float32(centers)
+
+    np.savetxt('descriptors/HOG/' + folder_name + '/' + folder_name + '.vocab', descs, delimiter=',')
