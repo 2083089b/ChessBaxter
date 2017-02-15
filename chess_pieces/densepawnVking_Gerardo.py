614a82d1f7c3dbda95f760067e2dc89f6b33a2eb
Lorenzo <lorys_23@hotmail.it>
2017-02-15 13:04:16 +0000

    New cropped pictures

--- chess_pieces/densepawnVking_Gerardo.py
+++ chess_pieces/densepawnVking_Gerardo.py
@@ -5,10 +5,10 @@ from extra_tools import extract_HOG, chess_train
 from scipy.stats import itemfreq
 
 # 'queen','knight','bishop','rock',
-clf_PKS = chess_train('descriptors/HOG', ['pawn', 'king', 'queen','knight','bishop','rock', 'square'], train_mod="LR")
+clf_PKS = chess_train('descriptors/HOG', ['pawn', 'king', 'square'], train_mod="SVM")
 # clf_PKS = chess_train('descriptors/HOG', ['pawn', 'king', 'square'], train_mod="LR")
-# clf_QkS = chess_train('descriptors/HOG', ['queen', 'knight', 'square'], train_mod="LR")
-# clf_BRS = chess_train('descriptors/HOG', ['bishop', 'rock', 'square'], train_mod="LR")
+clf_QkS = chess_train('descriptors/HOG', ['queen', 'knight', 'square'], train_mod="SVM")
+clf_BRS = chess_train('descriptors/HOG', ['bishop', 'rock', 'square'], train_mod="SVM")
 print "Training done!"
 
 img = cv2.imread("king8.png")
@@ -28,26 +28,26 @@ des1 = np.float32(des1)
 
 # label = ["Pawn", "King", "Square", "Queen", "Knight", "Square", "Bishop", "Rook", "Square"]
 label = ["Pawn", "King", "Queen", "Knight", "Bishop", "Rook", "Square"]
-models = [clf_PKS]#, clf_QkS, clf_BRS]
+models = [clf_PKS, clf_QkS, clf_BRS]
 
 print ""
 print "************************"
 c_val = []
 c_prob = []
 for i, clf in enumerate(models):
-    prediction = clf.predict(des1)
-    freq = itemfreq(prediction)
-    c_val.append(freq[:,1].tolist())
-    print freq
-    pred_prob = clf.predict_proba(des1)
-    # print pred_prob
-    print "************************"
-    out = pred_prob.mean(axis=0)
-    print "Out: " + str(out)
-    c_prob.append(out.tolist())
-    print "************************"
-    print "************************"
-    print ""
+	prediction = clf.predict(des1)
+	freq = itemfreq(prediction)
+	c_val.append(freq[:,1].tolist())
+	print freq
+	pred_prob = clf.predict_proba(des1)
+	# print pred_prob
+	print "************************"
+	out = pred_prob.mean(axis=0)
+	print "Out: " + str(out)
+	c_prob.append(out.tolist())
+	print "************************"
+	print "************************"
+	print ""
 
 # NOT WORKING: NEED TO FIX FREQUENCY ACC TO INCLUDE ALL CLASSES PER CLASSIFIER
 # freq_class = np.array(c_val).flatten()
