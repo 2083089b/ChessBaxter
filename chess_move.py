7cdf3ba5031962649c9f9800d762d9f2197392de
Lorenzo <lorys_23@hotmail.it>
2017-03-21 13:45:00 +0000

    Last commit before submitting

--- chess_move.py
+++ chess_move.py
@@ -1,6 +1,5 @@
 import chess
 import chess.uci
-import time
 
 
 
--- final_sliding_window.py
+++ final_sliding_window.py
@@ -2,7 +2,7 @@ import cv2
 import math
 
 #
-# Write to file 64 images representing each square of the chessboard.
+# Write to HDD 64 images representing each square of the chessboard.
 # These images will then be used by tensorflow to predict which piece,
 # if any, sits on them.
 def final_sliding_window(img, lines_coordinates, colour_img):
@@ -23,15 +23,16 @@ def final_sliding_window(img, lines_coordinates, colour_img):
 		mean_height = int(round((pt1[1]+pt2[1])/2))
 		c += 2
 
-		pt3 = int(round(pt1[0]))
-		pt4 = int(round(pt1[1]))
-		pt5 = int(round(pt2[0]))
-		pt6 = int(round(pt2[1]))
-		first_half_length_of_line = math.sqrt((mid_point_average - pt5)**2 + (mean_height - pt6)**2)
-		second_half_length_of_line = math.sqrt((pt3 - mid_point_average)**2 + (pt4 - mean_height)**2)
+		x2 = int(round(pt1[0]))
+		y2 = int(round(pt1[1]))
+		x1 = int(round(pt2[0]))
+		y1 = int(round(pt2[1]))
+		# Eucleadian distance
+		first_half_length_of_line = math.sqrt((mid_point_average - x1)**2 + (mean_height - y1)**2)
+		second_half_length_of_line = math.sqrt((x2 - mid_point_average)**2 + (y2 - mean_height)**2)
 		first_step = int(round(first_half_length_of_line/4))
 		second_step = int(round(second_half_length_of_line/4))
-		current_point = pt5
+		current_point = x1
 		c2 = 0
 		points = []
 		while c2 < 5:
@@ -54,17 +55,14 @@ def final_sliding_window(img, lines_coordinates, colour_img):
 				sliding_window = img[mean_height-110:mean_height, points[c3][0]:points[c3+1][0]]
 				colour_sliding_window = colour_img[mean_height-110:mean_height, points[c3][0]:points[c3+1][0]]
 				half_sliding_window = colour_img[mean_height-55:mean_height, points[c3][0]:points[c3+1][0]]
-				# cv2.imshow('',colour_sliding_window)
-				# cv2.waitKey(0)
-				# cv2.destroyAllWindows()
 
 			else:
 				sliding_window = img[mean_height-130:mean_height, points[c3][0]:points[c3+1][0]]
 				colour_sliding_window = colour_img[mean_height-130:mean_height, points[c3][0]:points[c3+1][0]]
 				half_sliding_window = colour_img[mean_height-70:mean_height, points[c3][0]:points[c3+1][0]]
+			print c
 			# cv2.imshow("sliding_window",sliding_window)
 			# cv2.waitKey(0)
-			# cv2.imwrite('sliding_windows/sliding_window'+str(global_counter)+".jpg",sliding_window)
 			cv2.imwrite('sliding_windows/sliding_window'+str(global_counter)+".jpg",colour_sliding_window)
 			cv2.imwrite('sliding_windows/halves/sliding_window'+str(global_counter)+".jpg",half_sliding_window)
 			c3 += 1
